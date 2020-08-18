a179631a288386733659ca61588e24b8645b085c
Erkin Pardayev <erkin.pardayev@gmail.com>
2020-08-18 12:07:28 +0800

    Stage 1 completed

---
+++ Web Crawler/task/src/crawler/WebCrawler.java
@@ -0,0 +1,26 @@
+package crawler;
+
+import javax.swing.*;
+
+public class WebCrawler extends JFrame {
+    public WebCrawler() {
+        setTitle("Simple Window");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 300);
+        setVisible(true);
+        showTextArea();
+    }
+
+    public void showTextArea()
+    {
+        JTextArea TextArea = new JTextArea("HTML code?", 12, 18);
+        JPanel contents = new JPanel();
+        contents.add(new JScrollPane(TextArea));
+        setContentPane(contents);
+
+        TextArea.setLineWrap(true);
+        TextArea.setWrapStyleWord(true);
+        TextArea.setName("TextArea");
+        TextArea.disable();
+    }
+}
