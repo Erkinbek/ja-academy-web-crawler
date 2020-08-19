8b9f1e4994d05b44c03dcd8a70e3fc57ededd4b7
Erkin Pardayev <erkin.pardayev@gmail.com>
2020-08-19 18:33:54 +0800

    Completed stage #2

--- Problems/Convert to bytes/src/Main.java
+++ Problems/Convert to bytes/src/Main.java
@@ -1,4 +1,5 @@
 import java.io.InputStream;
+import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) throws Exception {
--- Web Crawler/task/src/crawler/WebCrawler.java
+++ Web Crawler/task/src/crawler/WebCrawler.java
@@ -1,27 +1,74 @@
 package crawler;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 public class WebCrawler extends JFrame {
+
+    public String source;
+
     public WebCrawler() {
-        setTitle("Simple Window");
+        setTitle("Web Crawler");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(300, 300);
+        setSize(650, 600);
+
+        JPanel contents = new JPanel();
+
+        JTextArea textArea = textArea();
+        JTextField textField = textField();
+
+        contents.add(textField);
+        contents.add(button(textArea, textField));
+        contents.add(new JScrollPane(textArea));
+
+        setContentPane(contents);
+
         setVisible(true);
-        showTextArea();
     }
 
-    public void showTextArea()
-    {
-        JTextArea TextArea = new JTextArea("HTML code?", 12, 18);
-        JPanel contents = new JPanel();
-        contents.add(new JScrollPane(TextArea));
-        setContentPane(contents);
+    public JTextField textField() {
+        JTextField textField = new JTextField(45);
+        textField.setName("UrlTextField");
+        textField.setVisible(true);
+        return textField;
+    }
+
+    public JButton button(JTextArea textArea, JTextField textField) {
+        JButton button = new JButton("Get text!");
+        button.setName("RunButton");
+        button.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                textArea.setText(getSource(textField.getText()));
+            }
+        });
+        return button;
+    }
+
+
+    public JTextArea textArea() {
+        JTextArea TextArea = new JTextArea(30, 52);
 
         TextArea.setLineWrap(true);
         TextArea.setWrapStyleWord(true);
-        TextArea.setName("TextArea");
+        TextArea.setName("HtmlTextArea");
         TextArea.disable();
+        return TextArea;
+    }
+
+    public String getSource(String url) {
+        try (InputStream inputStream = new BufferedInputStream(new URL(url).openStream())) {
+            return new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "Failed";
     }
 }
